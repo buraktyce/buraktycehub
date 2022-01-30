47ae3a4369164c3f188f0f4a68a2d21dc3bfb81d
buraktyce <97315217+buraktyce@users.noreply.github.com>
2022-01-30 21:28:43 +0300

    Add files via upload

---
+++ buraktycehub.py
@@ -0,0 +1,52 @@
+import webbrowser
+
+print("#####################################")
+print("                                     ")
+print("             buraktycehub            ")
+print("         instagram : buraktyce       ")
+print("         VPN açık tutun aq           ")
+print("                                     ") 
+print("#####################################")
+print()
+
+print("1.pornhub anasayfa")
+print("2.pornhub bdsm")
+print("3.xnxx bdsm ")
+print("4.xnxx bdsm 2")
+print("5.doeda anasayfa")
+print("6.xnxx anasayfa")
+print("7.tüm porn siteleri ")
+print("8.xvideos anasayfa")
+print("9.tubebdsm anasayfa")
+print()
+
+
+
+porn = input("İşlem Numarası:")
+
+if porn =="1":
+    webbrowser.open_new("https://www.pornhub.com/")
+
+if porn =="2":
+    webbrowser.open_new("https://www.pornhub.com/video/search?search=bdsm+vibrator")
+
+if porn =="3":
+    webbrowser.open_new("https://www.xnxx.com/search/bdsm+vibrat%C3%B6r")
+
+if porn =="4":
+    webbrowser.open_new("https://www.xnxx.com/search/bdsm+orgasm")
+
+if porn =="5":
+    webbrowser.open_new("http://www.doeda.com/")
+
+if porn =="6":
+    webbrowser.open_new("https://www.xnxx.com/")
+
+if porn =="7":
+    webbrowser.open_new("https://theporndude.com/")
+
+if porn =="8":
+    webbrowser.open_new("https://www.xvideos.com/")
+
+if porn =="9":
+    webbrowser.open_new("https://www.tubebdsm.com/")
